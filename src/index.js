c40d683b2cefc37c4649bafad4f840e59160b1f0
Galina Muzychenko <muzychenko.galina@gmail.com>
2020-10-14 17:13:11 +0300

    fix: chek() function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var arr =bracketsConfig[0];
+   
+    let temp = str;
+    let reg ="";
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+
+        
+        temp = temp.replace(bracketsConfig[i].join(''), '');
+     
+    }
+
+  if (temp === str) {
+      return false;
+  } else if (temp === '') {
+      return true;
+  } else return check(temp, bracketsConfig); 
 }
